d12f094effad9eff766ea8c7a2d50d6be8050065
anson <ansonbenny16@gmail.com>
2023-04-13 19:58:13 +0530

    bug fix

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ import dotnet from 'dotenv'
 import { connectDB } from './db/connection.js'
 import ChatRoute from './routes/chat.js'
 import UserRoute from './routes/user.js'
+import path from 'path'
 
 dotnet.config()
 
@@ -18,8 +19,14 @@ app.use(cors({ credentials: true, origin: process.env.SITE_URL }))
 app.use(cookieParser())
 app.use(express.json({ limit: '50mb' }))
 
-app.use('/api/chat', ChatRoute)
-app.use('/api/user', UserRoute)
+// api route
+app.use('/api/chat/', ChatRoute)
+app.use('/api/user/', UserRoute)
+
+// front end react route
+app.get('/*',(req,res)=>{
+    res.sendFile(path.join(`${path.resolve(path.dirname(''))}/dist/index.html`))
+})
 
 connectDB((err) => {
     if (err) return console.log("MongoDB Connect Failed : ", err)
